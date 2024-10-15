3bf7e5e4a51d12b4ca9a79db5a6354e01cd36dba
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-15 18:08:58 +0600

    added a 6th bascic concept osf java script

---
+++ fundamental.js
@@ -0,0 +1,42 @@
+// 1 How to declear a variabble using let and constant
+
+const fathersName = "Arnold"
+let sesson = 'rainy';
+sesson = 'winter'
+
+// 2  6 Condition <, >, === , !==, <=, >=
+//   Multiple condition : $$  ||
+
+if(fathersName === 'arnold' || sesson === 'rainy'){
+  
+}
+else if(fathersName = 'Arnold'){
+ 
+}
+else{
+
+}
+
+
+// 3 Array  declear
+// index
+// length, push
+const numbers = [20, 32, 65, 45]
+// numbers [0] = 26
+
+// 4 for loop, while loop
+
+for(let i = 0; i<numbers.length; i++){
+  const number = numbers[i]
+  console.log(number)
+}
+
+// 5 function
+
+function multiply(num1, num2){
+    const result = num1 * num2
+    return result
+}
+
+const output = multiply(35, 78)
+console.log(output)

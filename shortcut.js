568dabe978304e5cccc5af5a50d85717d4c7e930
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-16 16:05:41 +0600

    added a true, falsy Ternary and shotcchut.js

---
+++ shortcut.js
@@ -0,0 +1,34 @@
+let myVar = 5
+
+// check truthy
+if(myVar){
+    myVar = myVar * 100
+}
+else{
+    myVar = 0
+
+}
+
+// check negative or falsy
+
+let myMoney = 50
+
+if(!myMoney){
+
+}
+
+const money = 800
+let food;
+
+if(money > 100){
+    food = "biryni khabo"
+}
+else{
+    food = " cha buscuit khabo"
+}
+console.log(food)
+
+// Ternary operator
+
+let food1 = money>100 ? 'biryni khabo' : 'chaBuiscut khabo'
+console.log(food1)
